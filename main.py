ea32609a876faf659cbdf0a8442dbf82201d6a01
yymin1022 <yymin1022@gmail.com>
2022-12-23 05:15:34 +0900

    Update main.py : Added Internal Document (Comments)

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ runtimeStack = []
 def main(argv):
     global code, mainIdx, runtimeStack
 
+    # Read Script File
     scriptFile = open(argv[1], "r")
 
     for curLine in scriptFile.readlines():
@@ -16,19 +17,24 @@ def main(argv):
 
     scriptFile.close()
 
+    # Call Function for Splitting Functions
     getFunctions()
 
+    # Check if Main Function Exist
     if mainIdx < 0:
         print("No Starting Function")
         return
 
+    # Split Each Splitted Functions to Statements
     for idx in range(len(functions)):
         checkFunctions(idx)
 
+    # Add Main Function to Runtime Stack
     runtimeStack.append({functions[mainIdx][0]: {"DL": -1, "RA": ""}})
     runFunction(mainIdx)
 
 
+# Split Each Functions
 def getFunctions():
     global code, functions, mainIdx
 
@@ -42,23 +48,28 @@ def getFunctions():
         functionAnalyze[0] = functionAnalyze[0].rstrip()
         functionAnalyze[1] = functionAnalyze[1].rstrip()
 
+        # Check if this Function is Main Function
         if functionAnalyze[0] == "main":
             mainIdx = i
 
         functions[i] = [functionAnalyze[0], functionAnalyze[1], []]
 
+        # Exit Program if Duplicated Function Name
         for j in range(len(functions)):
             if i != j and functions[j][0] == functions[i][0]:
                 print(f"Duplicate Declaration of the Function Name : {functions[i][0]}")
                 exit(0)
 
 
+
+# Split Function Body to Statements
 def checkFunctions(idx):
     global functions
 
     functions[idx][1] = functions[idx][1].split(";")
 
 
+# Define Variables from variable Statements
 def defVariables(statement, idx):
     global functions, runtimeStack
 
@@ -66,11 +77,13 @@ def defVariables(statement, idx):
         if word[-1] == ",":
             word = word[:-1]
 
+        # Check if Duplicated Name with Other Identifier or Function
         for i in range(len(functions)):
             if word == functions[i][0]:
                 print(f"Duplicate Declaration of the Identifier or the Function Name : {word}")
                 exit(0)
 
+        # Check if Duplicated NAme with Other Identifier
         isVarOK = True
         for i in functions[idx][2]:
             if i == word:
@@ -79,9 +92,11 @@ def defVariables(statement, idx):
         if isVarOK:
             functions[idx][2].append(word)
 
+    # Add Local Variables to Runtime Stack
     runtimeStack[-1][functions[idx][0]]["LV"] = functions[idx][2]
 
 
+# Run Each Statements in Function
 def runFunction(idx):
     global functions
 
@@ -91,17 +106,22 @@ def runFunction(idx):
         if stmt != "":
             runStatement(idx, stmt)
 
+    # If Function Ended, Pop from Runtime Stack
     runtimeStack.pop()
 
 
+# Run Statement
 def runStatement(idx, stmt):
     global functions, runtimeStack
 
     stmt = stmt.split(" ")
 
     if stmt[0] == "variable":
+        # If variable Keyword, start Defining Variable
         defVariables(stmt[1:], idx)
     elif stmt[0] == "call":
+        # If call Keyword, start Calling Function
+        # Get Current Dynamic Link and start Add New Function to Runtime Stack
         curDL = 0
         if runtimeStack[-1][functions[idx][0]]["DL"] >= 0:
             curDL = runtimeStack[-1][functions[idx][0]]["DL"] + len(list(runtimeStack[runtimeStack[-1][functions[idx][0]]['DL']].values())[0]['LV'])
@@ -113,10 +133,12 @@ def runStatement(idx, stmt):
                 runtimeStack.append({functions[i][0]: {"RA": functions[idx][0], "DL": curDL}})
                 runFunction(i)
 
+        # Check if Undefined Function is Called
         if not isFunctionOK:
             print(f"Call to Undefined Function : {stmt[1]}")
             exit(0)
     elif stmt[0] == "print_ari":
+        # If print_ari Keyword, Print Runtime Stack Content
         for runtime in reversed(runtimeStack):
             for key in runtime:
                 print(f"{key}:")
@@ -128,6 +150,7 @@ def runStatement(idx, stmt):
                     print(f"  Dynamic Link: {runtime[key]['DL']}")
                     print(f"  Return Address: {runtime[key]['RA']}")
     else:
+        # If just a Variable Keyword
         varFrom = functions[idx][0]
         varName = stmt[0]
         varLinkCnt = 0
@@ -147,6 +170,7 @@ def runStatement(idx, stmt):
                 break
             varLinkCnt += 1
 
+        # Check if Undefined Variable is Called
         if not isVarFound:
             print(f"Call to Undefined Variable : {varName}")
             exit(0)
